0866e70975db4068dbd3ec4f7f0546c9f763ecc7
Rod-S <rodsanati@gmail.com>
2021-05-04 23:10:10 -0500

    complete reverse string soln

--- basicAlgorithmScripting.js
+++ basicAlgorithmScripting.js
@@ -5,11 +5,24 @@
 // equivalent to the given Celsius temperature. Use the algorithm mentioned above to
 // help convert the Celsius temperature to Fahrenheit.
 
-function convertToF(celsius) {
-    let fahrenheit;
-    fahrenheit = celsius * (9/5) + 32;
-    return fahrenheit;
+// function convertToF(celsius) {
+//     let fahrenheit;
+//     fahrenheit = celsius * (9/5) + 32;
+//     return fahrenheit;
+// }
+//
+// console.log(convertToF(30));;
+
+// Reverse the provided string.
+//     You may need to turn the string into an array before you can reverse it.
+//     Your result must be a string.
+
+function reverseString(str) {
+    let strArr;
+    let revStr;
+    strArr = str.split("");
+    revStr = strArr.reverse().join("").toString();
+    return revStr;
 }
 
-convertToF(30);
-
+console.log(reverseString("hello"));;
