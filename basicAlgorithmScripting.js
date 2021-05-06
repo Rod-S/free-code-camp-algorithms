6e5d006eda94d36ff49fd000872c5623c8fca721
Rod-S <rodsanati@gmail.com>
2021-05-06 00:50:13 -0500

    complete truncate string solution

--- basicAlgorithmScripting.js
+++ basicAlgorithmScripting.js
@@ -113,4 +113,17 @@ function repeatStringNumTimes(str, num) {
     return repStr;
 }
 
-repeatStringNumTimes("abc", 3);
+// repeatStringNumTimes("abc", 3);
+
+// Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
+
+function truncateString(str, num) {
+    if (str.length > num) {
+        let truncStr = str.slice(0, num) + "...";
+        return truncStr;
+    }
+    return str;
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
+truncateString("Peter Piper picked a peck of pickled peppers", 11)
