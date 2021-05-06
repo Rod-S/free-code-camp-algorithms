9b39e8b8c70650be0ba007d58355f610476c8d02
Rod-S <rodsanati@gmail.com>
2021-05-06 00:22:23 -0500

    complete largestOfFour solution

--- basicAlgorithmScripting.js
+++ basicAlgorithmScripting.js
@@ -67,4 +67,24 @@ function findLongestWordLength(str) {
     return longestWord.length;
 }
 
-findLongestWordLength("The quick brown fox jumped over the lazy dog");
+// findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+// Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+//
+//     Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+
+function largestOfFour(arr) {
+    let lgArr = [];
+    arr.forEach((array, i) => {
+        let currLg = 0;
+        arr[i].forEach((val) => {
+            if (val > currLg) {
+                currLg = val;
+            }
+        })
+        lgArr.push(currLg);
+        });
+    return lgArr;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
