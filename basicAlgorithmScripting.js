36129409221d6236f5f6d6918ff4fa9cd3d716c1
Rod-S <rodsanati@gmail.com>
2021-05-08 00:01:57 -0500

    complete titleCase solution

--- basicAlgorithmScripting.js
+++ basicAlgorithmScripting.js
@@ -162,4 +162,20 @@ function booWho(bool) {
 
 booWho(null);
 
-console.log(booWho(null));;
+// console.log(booWho(null));;
+
+// Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+//
+// For the purpose of this exercise, you should also capitalize connecting words like the and of.
+
+function titleCase(str) {
+    let strArr = str.split(" ");
+    strArr = strArr.map(e=>
+        e.charAt(0).toUpperCase() + e.slice(1, e.length).toLowerCase()
+    );
+    strArr = strArr.join(" ");
+    return strArr;
+}
+
+
+console.log(titleCase("I'm a little tea pot"));;
