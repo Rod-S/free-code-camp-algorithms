86732a8c9b030ab25f805aaa44a4d9c9d6c57f2f
Rod-S <rodsanati@gmail.com>
2021-05-18 23:01:05 -0500

    complete array splice exercise

--- basicAlgorithmScripting.js
+++ basicAlgorithmScripting.js
@@ -178,4 +178,25 @@ function titleCase(str) {
 }
 
 
-console.log(titleCase("I'm a little tea pot"));;
+// console.log(titleCase("I'm a little tea pot"));
+
+// You are given two arrays and an index.
+//
+//     Copy each element of the first array into the second array, in order.
+//
+//     Begin inserting elements at index n of the second array.
+//
+//     Return the resulting array. The input arrays should remain the same after the function runs.
+
+function frankenSplice(arr1, arr2, n) {
+    let arr1mod =  arr1;
+    let arr2mod = [...arr2];
+    arr1mod.forEach((element, index) => {
+        arr2mod.splice((n+index), 0, element)
+    });
+    console.log(arr2mod);
+    console.log(arr2);
+    return arr2mod;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
