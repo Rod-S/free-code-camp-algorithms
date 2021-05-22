fc9c8ada4e270eb9c5b3a92833740aec4b2a2526
Rod-S <rodsanati@gmail.com>
2021-05-21 23:54:03 -0500

    basic algorithm scripting

--- basicAlgorithmScripting.js
+++ basicAlgorithmScripting.js
@@ -236,3 +236,37 @@ console.log(bouncer([7, "ate", "", false, 9]));
 console.log(bouncer(["a", "b", "c"]));
 console.log(bouncer([false, null, 0, NaN, undefined, ""]));
 console.log(bouncer([null, NaN, 1, 2, undefined]));
+
+// Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+//
+//     For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+//
+// Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1).
+
+function getIndexToIns(arr, num) {
+    return num;
+}
+
+getIndexToIns([40, 60], 50);
+
+// Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+//
+//     For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+//
+// The arguments ["hello", "hey"] should return false because the string hello does not contain a y.
+//
+//     Lastly, ["Alien", "line"], should return true because all of the letters in line are present in Alien.
+
+function mutation(arr) {
+    return arr;
+}
+
+mutation(["hello", "hey"]);
+
+// Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+
+function chunkArrayInGroups(arr, size) {
+    return arr;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
